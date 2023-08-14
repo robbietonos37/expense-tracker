91e335a5e90f1d6fc370d060a9b71c6b431b79ff
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-08-13 23:21:32 -0500

    Finish up basic expense tracker

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const categorySelect = document.getElementById('category-select')
 const amountInput = document.getElementById('amount-input')
 const dateInput = document.getElementById('date-input')
 const addBtn = document.getElementById('add-btn')
-const expensesTableBody = document.getElementById('expenses-table-body')
+const expensesTableBody = document.getElementById('expense-table-body')
 const totalAmountCell = document.getElementById('total-amount')
 
 addBtn.addEventListener('click', function () {
@@ -23,8 +23,9 @@ addBtn.addEventListener('click', function () {
     }
     if (date === '') {
         alert('Please select a date')
+        return
     }
-    expenses.push(category, amount, date)
+    expenses.push({ category, amount, date })
 
     totalAmount += amount
     totalAmountCell.textContent = totalAmount
@@ -54,3 +55,15 @@ addBtn.addEventListener('click', function () {
     deleteCell.appendChild(deleteBtn)
 
 })
+
+for (const expense of expenses) {
+    totalAmount += expense.amount
+    totalAmountCell.textContent = totalAmount
+
+    const newRow = expensesTableBody.insertRow()
+    const categoryCell = newRow.insertCell()
+    const amountCell = newRow.insertCell()
+    const dateCell = newRow.insertCell()
+    const deleteCell = newRow.insertCell()
+    const deleteBtn = document.createElement('button')
+}
