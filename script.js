7384c265d658990511568e09a9db656f3a39c46c
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-08-13 23:40:18 -0500

    Add semicolons and deposits array

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let expenses = [];
+let deposits = [];
 let totalAmount = 0;
 
-const categorySelect = document.getElementById('category-select')
-const amountInput = document.getElementById('amount-input')
-const dateInput = document.getElementById('date-input')
-const addBtn = document.getElementById('add-btn')
-const expensesTableBody = document.getElementById('expense-table-body')
-const totalAmountCell = document.getElementById('total-amount')
+const categorySelect = document.getElementById('category-select');
+const amountInput = document.getElementById('amount-input');
+const dateInput = document.getElementById('date-input');
+const addBtn = document.getElementById('add-btn');
+const expensesTableBody = document.getElementById('expense-table-body');
+const totalAmountCell = document.getElementById('total-amount');
 
 addBtn.addEventListener('click', function () {
     const category = categorySelect.value;
@@ -14,56 +15,56 @@ addBtn.addEventListener('click', function () {
     const date = dateInput.value;
 
     if (category === '') {
-        alert('Please select a category')
+        alert('Please select a category');
         return
     }
     if (isNaN(amount) || amount <= 0) {
-        alert('Please enter a valid amount')
+        alert('Please enter a valid amount');
         return
     }
     if (date === '') {
-        alert('Please select a date')
+        alert('Please select a date');
         return
     }
-    expenses.push({ category, amount, date })
+    expenses.push({ category, amount, date });
 
-    totalAmount += amount
-    totalAmountCell.textContent = totalAmount
-    const newRow = expensesTableBody.insertRow()
+    totalAmount += amount;
+    totalAmountCell.textContent = totalAmount;
+    const newRow = expensesTableBody.insertRow();
 
-    const categoryCell = newRow.insertCell()
-    const amountCell = newRow.insertCell()
-    const dateCell = newRow.insertCell()
-    const deleteCell = newRow.insertCell()
-    const deleteBtn = document.createElement('button')
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
 
-    deleteBtn.textContent = 'Delete'
-    deleteBtn.classList.add('delete-btn')
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
     deleteBtn.addEventListener('click', function () {
-        expenses.splice(expenses.indexOf(expense), 1)
+        expenses.splice(expenses.indexOf(expense), 1);
 
-        totalAmount -= expense.amount
-        totalAmountCell.textContent = totalAmount
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
 
-        expensesTableBody.removeChild(newRow)
+        expensesTableBody.removeChild(newRow);
     })
 
-    const expense = expenses[expenses.length - 1]
-    categoryCell.textContent = expense.category
-    amountCell.textContent = expense.amount
-    dateCell.textContent = expense.date
-    deleteCell.appendChild(deleteBtn)
+    const expense = expenses[expenses.length - 1];
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(deleteBtn);
 
 })
 
 for (const expense of expenses) {
-    totalAmount += expense.amount
-    totalAmountCell.textContent = totalAmount
+    totalAmount += expense.amount;
+    totalAmountCell.textContent = totalAmount;
 
-    const newRow = expensesTableBody.insertRow()
-    const categoryCell = newRow.insertCell()
-    const amountCell = newRow.insertCell()
-    const dateCell = newRow.insertCell()
-    const deleteCell = newRow.insertCell()
-    const deleteBtn = document.createElement('button')
+    const newRow = expensesTableBody.insertRow();
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
 }
