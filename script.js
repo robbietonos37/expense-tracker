0596bad2203f9def2783540c820db6177c899617
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-08-13 19:25:36 -0500

    Create js elements for html elements

---
+++ script.js
@@ -0,0 +1,9 @@
+let expenses = [];
+let totalAmount = 0;
+
+const categorySelect = document.getElementById('category-select')
+const amountInput = document.getElementById('amount-input')
+const dateInput = document.getElementById('date-input')
+const addBtn = document.getElementById('add-btn')
+const expensesTableBody = document.getElementById('expenses-table-body')
+const totalAmountCell = document.getElementById('total-amount')
